dd73cb40f62eb85b38525ef6b419739d6dc501b5
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-29 10:37:09 +0200

    Add equals function

--- main.py
+++ main.py
@@ -9,7 +9,28 @@ def button_press(num):
     equation_label.set(equation_text)
 
 def equals():
-    pass
+
+    global equation_text
+
+    try:
+
+        total = str(eval(equation_text))
+
+        equation_label.set(total)
+
+        equation_text = total
+
+    except SyntaxError:
+
+        equation_label.set("syntax error")
+
+        equation_text = ""
+
+    except ZeroDivisionError:
+
+        equation_label.set("arithmetic error")
+
+        equation_text = ""
 
 def clear():
     pass
