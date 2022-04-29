55add5768ab26c13d510c3518ffe18ed48e2286a
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-29 10:28:38 +0200

    Add button 4 - 6

--- main.py
+++ main.py
@@ -32,4 +32,13 @@ button2.grid(row=0, column=1)
 button3 = Button(frame, text=3, height=4, width=9, font=35, command=lambda: button_press(3))
 button3.grid(row=0, column=2)
 
+button4 = Button(frame, text=4, height=4, width=9, font=35, command=lambda: button_press(4))
+button4.grid(row=1, column=0)
+
+button5 = Button(frame, text=5, height=4, width=9, font=35, command=lambda: button_press(5))
+button5.grid(row=1, column=1)
+
+button6 = Button(frame, text=6, height=4, width=9, font=35, command=lambda: button_press(6))
+button6.grid(row=1, column=2)
+
 window.mainloop()
