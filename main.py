e48edb5a9306df719362d1fe193b6069342204c9
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-29 10:37:51 +0200

    Add clear function

--- main.py
+++ main.py
@@ -33,7 +33,12 @@ def equals():
         equation_text = ""
 
 def clear():
-    pass
+
+    global equation_text
+
+    equation_label.set("")
+
+    equation_text = ""
 
 window = Tk()
 window.title(" Calculator ")
