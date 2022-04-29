4859f1c3cdad1ed3d3e4f440f2f22c65c5997631
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-29 10:30:51 +0200

    Add button plus and minus

--- main.py
+++ main.py
@@ -53,4 +53,10 @@ button9.grid(row=2, column=2)
 button0 = Button(frame, text=0, height=4, width=9, font=35, command=lambda: button_press(0))
 button0.grid(row=3, column=0)
 
+plus = Button(frame, text='+', height=4, width=9, font=35, command=lambda: button_press('+'))
+plus.grid(row=0, column=3)
+
+minus = Button(frame, text='-', height=4, width=9, font=35, command=lambda: button_press('-'))
+minus.grid(row=1, column=3)
+
 window.mainloop()
