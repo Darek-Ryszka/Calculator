2f1e14b3e3367338e409fd674aa298f2d33cd844
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-29 10:23:49 +0200

    Add window structure

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ def equals():
 
 def clear():
     pass
+
+window = Tk()
+window.title(" Calculator ")
+window.geometry("400x400")
+
+equation_text = ""
+
+equation_label = StringVar()
+
+label = Label(window, textvariable=equation_label, font=('consolas',20), bg="white", width=24, height=2)
+label.pack()
+
+window.mainloop()
